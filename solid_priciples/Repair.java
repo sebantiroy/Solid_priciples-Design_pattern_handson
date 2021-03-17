7915376e45d5f67accbaf9ffbba4b17820ef120b
sebantiroy <80837074+sebantiroy@users.noreply.github.com>
2021-03-17 20:56:00 +0530

    Create Repair.java

---
+++ solid_priciples/Repair.java
@@ -0,0 +1,13 @@
+package solid_priciples;
+
+public class Repair implements I_repair{
+
+	@Override
+	public void ProcessPhoneRepair(String Modelname) {
+		// TODO Auto-generated method stub
+		System.out.println("repair accepted"+Modelname);
+	}
+
+	
+
+}
