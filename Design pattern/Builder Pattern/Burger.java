42c64ca852177496e33d30c1360d717bbd0d3718
sebantiroy <80837074+sebantiroy@users.noreply.github.com>
2021-03-18 16:59:04 +0530

    Create Burger.java

---
+++ Design pattern/Builder Pattern/Burger.java
@@ -0,0 +1,16 @@
+package BuilderPattern;
+
+public abstract class Burger implements Item {
+
+@Override
+
+public Packing packing() {
+
+return new Wrapper();
+
+}
+
+@Override
+
+public abstract float price();
+}
