147c3a4a91e33924605a4a53a1bafae528240e62
sebantiroy <80837074+sebantiroy@users.noreply.github.com>
2021-03-18 19:46:43 +0530

    Create Square.java

---
+++ Design pattern/Facade Pattern/Square.java
@@ -0,0 +1,11 @@
+package FacadePattern;
+
+public class Square implements Shape {
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("Square is drawn");
+	}
+
+}
