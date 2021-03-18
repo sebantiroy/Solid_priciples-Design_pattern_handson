ba3f9c41a5e1140f009a7352073a8c3ce49920f4
sebantiroy <80837074+sebantiroy@users.noreply.github.com>
2021-03-18 17:00:55 +0530

    Create ColdDrink.java

---
+++ Design pattern/Builder Pattern/ColdDrink.java
@@ -0,0 +1,17 @@
+package BuilderPattern;
+
+public abstract class ColdDrink implements Item {
+
+@Override
+
+public Packing packing() {
+
+return new Bottle();
+
+}
+
+@Override
+
+public abstract float price();
+
+}
